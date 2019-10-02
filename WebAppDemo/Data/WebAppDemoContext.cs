965327c850b16b2d1d19a961d4c054f6aa733873
TEDDY TEASDALE <T7091808@live.tees.ac.uk>
2019-10-02 11:20:19 +0100

    hhbjhb

---
+++ WebAppDemo/Data/WebAppDemoContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebAppDemo.Models
+{
+    public class WebAppDemoContext : DbContext
+    {
+        public WebAppDemoContext (DbContextOptions<WebAppDemoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebAppDemo.Models.Movies> Movies { get; set; }
+    }
+}
